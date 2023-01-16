261165cc0ad94a1158af4f98c2ad802b4b518ac2
Dmitriy Pischik <pischik@bk.ru>
2023-01-16 21:10:04 +1000

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+string is_weekend(int day_number)
+{
+    string response = "";
+    if (day_number == 6 || day_number == 7) response = "День недели с этим номером - выходной";
+    else if (day_number < 0 || day_number > 7 ) response = "Нет дня недели с таким номером";
+    else response = "День недели с этим номером - невыходной";
+    return response;
+}
+
+Console.Write("Введите номер недели: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+ Console.WriteLine(is_weekend(x));
